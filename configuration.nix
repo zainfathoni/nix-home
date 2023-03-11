7b2b7b6ceb1230911dca55c17f22eabe36e94b35
Zain Fathoni <me@zainf.dev>
2023-03-12 00:33:02 +0800

    feat(nix-shell): fix `nix-shell -p` not working

--- configuration.nix
+++ configuration.nix
@@ -6,11 +6,13 @@
   nix.useDaemon = true;
 
   # Configure extra options: https://nix-community.github.io/home-manager/options.html#opt-nix.extraOptions
-  # Storage optimization: https://nixos.wiki/wiki/Storage_optimization
-  # Enable flakes permanently: https://nixos.wiki/wiki/Flakes#Permanent
+  # `auto-optimise-store` | Storage optimization: https://nixos.wiki/wiki/Storage_optimization
+  # `experimental-features` | Enable flakes permanently: https://nixos.wiki/wiki/Flakes#Permanent
+  # `extra-nix-path` | Temporary fix for `nix-shell`: https://github.com/DeterminateSystems/nix-installer/pull/270
   nix.extraOptions = ''
     auto-optimise-store = true
     experimental-features = nix-command flakes
+    extra-nix-path = nixpkgs=flake:nixpkgs
   '';
 
   # Keyboard mapping
