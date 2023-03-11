312d9ac2e09d60886f6791bf17144de906a2871e
Zain Fathoni <me@zainf.dev>
2023-03-11 16:51:25 +0800

    feat(flake): initialize flake for aarch64-darwin
    nix flake init

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "Pejuang Kode flake";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
+
+    packages.aarch64-darwin.default = self.packages.aarch64-darwin.hello;
+
+  };
+}
