b7d942bcecb1b0ad1f2ef0a2bc4c4d25c7bcb37c
Zain Fathoni <me@zainf.dev>
2023-04-24 10:50:05 +0800

    feat(volta): define environment variables for Volta
    https://docs.volta.sh/guide/getting-started

--- home/packages.nix
+++ home/packages.nix
@@ -1,9 +1,21 @@
 { pkgs, ... }:
 
 {
+  # Environment variables to always set at login.
+  # https://nix-community.github.io/home-manager/options.html#opt-home.sessionVariables
+  home.sessionVariables = {
+    VOLTA_HOME = "$HOME/.volta";
+  };
+
+  # Extra directories to add to $PATH
+  # https://nix-community.github.io/home-manager/options.html#opt-home.sessionPath
+  home.sessionPath = [
+    # Volta
+    "$VOLTA_HOME/bin"
+  ];
+
   # The set of packages to appear in the user environment
   # https://nix-community.github.io/home-manager/options.html#opt-home.packages
   home.packages = with pkgs; [
   ];
 }
-
