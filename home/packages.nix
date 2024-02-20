c75db9ea2518848f601a27c04509b5cf13b24d52
Zain Fathoni <me@zainf.dev>
2024-02-20 10:33:09 +0800

    Add installed per-user packages binary to the PATH env var

--- home/packages.nix
+++ home/packages.nix
@@ -11,6 +11,14 @@
   # Extra directories to add to $PATH
   # https://nix-community.github.io/home-manager/options.html#opt-home.sessionPath
   home.sessionPath = [
+    # home-manager per-user profile
+    # This is supposed to be set automatically by the home-manager.useUserPackages flag
+    # However, it doesn't seem to be working for me
+    # Therefore, I'm adding it manually
+    # https://nix-community.github.io/home-manager/index.xhtml#sec-flakes-nix-darwin-module
+    # FIXME: Remove this once the issue is fixed
+    "/etc/profiles/per-user/zain/bin"
+
     # Volta
     "$VOLTA_HOME/bin"
 
