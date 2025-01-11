e5c0b6cc91554e608c30a403e673840df85bb8b4
Zain Fathoni <me@zainf.dev>
2025-01-11 22:13:41 +0800

    feat(config): add Zed configuration files and symlink to home assets

--- home/assets.nix
+++ home/assets.nix
@@ -14,4 +14,8 @@ in
 
   # Symlink Warp workflows
   home.file.".warp".source = mkOutOfStoreSymlink "${nixConfigDirectory}/assets/.warp";
+
+  # Symlink Zed configuration files
+  home.file.".config/zed/settings.json".source = mkOutOfStoreSymlink "${nixConfigDirectory}/assets/.config/zed/settings.json";
+  home.file.".config/zed/keymap.json".source = mkOutOfStoreSymlink "${nixConfigDirectory}/assets/.config/zed/keymap.json";
 }
